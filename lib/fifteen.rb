a51d5ba72e4f4797182424e38206e859c4c5320a
gregurton <gburt802@gmail.com>
2014-09-26 21:42:50 -0400

    Formatting cleanup and resolved all errors in pry

--- lib/fifteen.rb
+++ lib/fifteen.rb
@@ -16,33 +16,33 @@ class Game
   end
 
   def greeting
-    puts "Welcome to our game of 15. You and the computer will take   turns choosing one number at a time between 1 to 9. Someone wins if they have said three numbers that sum to 15. Once a number is chosen, it can't be used again this round. Note that any combination of three numbers you mentioned can be used."
+    puts "Welcome to our game of 15. You and the computer will take turns choosing one number at a time between 1 to 9. Someone wins if they have said three numbers that sum to 15. Once a number is chosen, it can't be used again this round. Note that any combination of three numbers you mentioned can be used."
   end
 
   # We could make these into classes
   # Need to put some way of randomly choosing who goes first.
   # Need a while statement. ex: while result.size < 1 && number pool !=[],
 
-  def player_turn#need argument here?
+  def player_turn #need argument here?
     puts "What number do you choose?"
-    gets.chomp_to.i = player_choice
+    gets.chomp.to_i = player_choice
     @number_pool.delete(player_choice)
     @player_numbers << player_choice
-    if
-      @player_numbers.size => 3
+
+    if @player_numbers >= 3
       @player_numbers.did_ye_win
     else
       computer_turn
     end
   end
 
+
   def computer_turn
     @number_pool.sample = computer_choice
     puts "The computer chose #{computer_choice}"
     @number_pool.delete(computer_choice)
     @computer_numbers << computer_choice
-    if
-      @computer_numbers.size => 3
+    if @computer_numbers.size >= 3
       @computer_numbers.did_ye_win
     else
       player_turn
@@ -61,14 +61,13 @@ class Game
 
   def sum_exists(numbers, sum)
     Array(numbers).combination(3).find_all { |x, y, z| x + y + z == sum } || []
+    result = sum_exists(numbers, sum)
+    puts "given this array of choices: #{numbers.inspect}"
+    puts "#{result.size} pairs add up to #{sum}: #{result.inspect}"
 
-  result = sum_exists(numbers, sum)
-
-  puts "given this array of choices: #{numbers.inspect}"
-  puts "#{result.size} pairs add up to #{sum}: #{result.inspect}"
-
-  if result.size => 1
-    puts "Win"
+    if result.size >= 1
+      puts "Win"
+    end
   end
 
     # We really want this to be three different options, player win, computer win, or no one win.
@@ -82,8 +81,8 @@ class Game
   #end
   # def is_the_game_unwinnable
   # end
-end
 
 # def number
 #   @number
 # end
+end

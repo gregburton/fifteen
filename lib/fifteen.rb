710822a0adf559920fe159aabf516e651332cc09
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2014-09-27 15:44:10 -0400

    no working scoring method, removed original method

--- lib/fifteen.rb
+++ lib/fifteen.rb
@@ -15,6 +15,15 @@ class Game
     @computer_numbers = []
   end
 
+# def winning_combinations(numbers, sum)
+  # Array(numbers).combination(3).find_all { |x, y, z| x + y + z == sum } || []
+  # result = winning_combinations(numbers, sum)
+#   puts "given this array of choices: #{numbers.inspect}"
+#   puts "#{result.size} pairs add up to #{sum}: #{result.inspect}"
+#   # if result.size >= 1
+#   #   puts "Win"
+#   # end
+# end
 
 # trying this down in the list of game methods
   # def greeting
@@ -32,7 +41,7 @@ class Game
     @player_numbers << player_choice
 
     if @player_numbers.size >= 3
-      @player_numbers.did_ye_win
+      score_player
     else
       computer_turn
     end
@@ -46,32 +55,27 @@ class Game
     @computer_numbers << computer_choice
     if @computer_numbers.size >= 3
       @computer_numbers.did_ye_win
+    # Array(@computer_numbers).combination(3).find_all { |x, y, z| x + y + z == 15 } || []
+    # puts "#{Array}"
+
     else
       player_turn
     end
   end
 
 # Only invoke did_ye_win after each player has three numbers. Need a counter for number of guesses.
-
-  def did_ye_win(numbers, sum)
-
-  # Right now, this is general, and we want it to apply to both. We also need to call this method on each player's set of guesses only, not the whole array of numbers. Do we need to make two different methods or call the method twice per round, once on computer and once on player?
-
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    sum = ARGV.empty? ? 15 : ARGV.first.to_i
-    # sum = 15
-  end
   #
-  def sum_exists(numbers, sum)
-    Array(numbers).combination(3).find_all { |x, y, z| x + y + z == sum } || []
-    result = sum_exists(numbers, sum)
-    puts "given this array of choices: #{numbers.inspect}"
-    puts "#{result.size} pairs add up to #{sum}: #{result.inspect}"
+  # def did_ye_win(numbers, sum)
+  # #
+  # # Right now, this is general, and we want it to apply to both. We also need to call this method on each player's set of guesses only, not the whole array of numbers. Do we need to make two different methods or call the method twice per round, once on computer and once on player?
+  # #
+  #   # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  #   @player_numbers = numbers
+  #   sum = ARGV.empty? ? 15 : ARGV.first.to_i
+  #
+  # end
+  # #
 
-    if result.size >= 1
-      puts "Win"
-    end
-  end
 
     # We really want this to be three different options, player win, computer win, or no one win.
     # if results.size > 1
@@ -96,6 +100,6 @@ game1 = Game.new
   puts  "Welcome to our game of 15. You and the computer will take turns choosing one number at a time between 1 to 9. Someone wins if they have said three numbers that sum to 15. Once a number is chosen, it can't be used again this round. Note that any combination of three numbers you mentioned can be used."
 game1.player_turn
 game1.computer_turn
-game1.did_ye_win
+# game1.did_ye_win
 game1.sum_exists
   puts "Game over"

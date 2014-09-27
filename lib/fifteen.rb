ef6f4754c83c5ff15b9abcec54fd4bcea085cb19
gregurton <gburt802@gmail.com>
2014-09-27 18:28:23 -0400

    Game currently doesn't end at you win. Keeps asking for more guesses.

--- lib/fifteen.rb
+++ lib/fifteen.rb
@@ -23,21 +23,18 @@ class Game
   # Need to put some way of randomly choosing who goes first.
   # Need a while statement. ex: while result.size < 1 && number pool !=[],
 
-# issues as of 4pm on 9/27:
-# need to define sum (called in score_player and score_computer)
+# issues:
 # could add logic if player does not specify a guess between one and nine
-     # if player_choice == 1..9
-       # puts "That is not a number between 1 and 9; please guess again."
-       # player_turn
-     #else
+# need to keep the game going if no one has won after 3 rounds AND also allow to end if someone wins
 
-  def player_turn #need argument here?
+  def player_turn
     puts "What number do you choose?"
     player_choice = gets.chomp.to_i
     @number_pool.delete(player_choice)
     @player_numbers << player_choice
       if @player_numbers.size >= 3
         score_player
+        computer_turn
       else
         computer_turn
       end
@@ -47,8 +44,9 @@ class Game
     play_num = @player_numbers.combination(3).to_a
     if play_num.any?{ |array| sum(array) == 15 }
       puts "You Win"
-      #NameError: undefined local variable or method `play_again' for #<Game:0x007ff86cb1ead8> from fifteen.rb:51:in `score_player'
-    else
+      play_again
+    # else
+    #   computer_turn
     end
   end
 
@@ -59,9 +57,7 @@ class Game
     @computer_numbers << computer_choice
     if @computer_numbers.size >= 3
       score_computer
-      # @computer_numbers.did_ye_win
-    # Array(@computer_numbers).combination(3).find_all { |x, y, z| x + y + z == 15 } || []
-    # puts "#{Array}"
+      player_turn
     else
       player_turn
     end
@@ -71,37 +67,26 @@ class Game
     comp_num = @computer_numbers.combination(3).to_a
     if comp_num.any?{ |array| sum(array) == 15}
       puts "Computer Wins"
-    else
+      play_again
+
+    # else
+    #   player_turn
+    end
+  end
+
+  def play_again
+    loop do
+      restart = Game.new
+      restart.player_turn
+      restart.computer_turn
+      # having player_turn and computer_turn mentioned here is (likely) necessary for game to run properly
+      puts "Game over. Would you like to play again? (Y)es or (N)o?"
+        player_response = gets.chomp.to_s
+        break if player_response.upcase == "N"
     end
   end
 end
 
-# Only invoke did_ye_win after each player has three numbers. Need a counter for number of guesses.
-  #
-  # def did_ye_win(numbers, sum)
-  #
-    # Right now, this is general, and we want it to apply to both. We also need to call this method on each player's set of guesses only, not the whole array of numbers. Do we need to make two different methods or call the method twice per round, once on computer and once on player?
-  #
-  #   # numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  #   @player_numbers = numbers
-  #   sum = ARGV.empty? ? 15 : ARGV.first.to_i
-  # end
-  #
-    # We really want this to be three different options, player win, computer win, or no one win.
-    # if results.size > 1
-    #   puts "Computer won."
-    # elsif  Array(@player_numbers).combination(3).find_all { |x, y, z| x + y + z == sum } || []
-    #   puts "Player won."
-    # else
-    #   puts "Keep trying" and makes game continue to play.
-    # end
-  # end
-  # def is_the_game_unwinnable
-  # end
-
-# def number
-#   @number
-# end
 loop do
   game1 = Game.new
   puts "Welcome to our game of 15. You and the computer will take turns choosing one number at a time between 1 to 9. Someone wins if they have said three numbers that sum to 15. Once a number is chosen, it can't be used again this round. Note that any combination of three numbers you mentioned can be used."
@@ -111,5 +96,4 @@ loop do
   puts "Game over. Would you like to play again? (Y)es or (N)o?"
     player_response = gets.chomp.to_s
     break if player_response.upcase == "N"
-      exit
 end
